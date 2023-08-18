366e300a90ceefe636c7deb53fc70de43f7db636
aiam0202 <aiam0202@gmail.com>
2023-08-18 14:12:56 +0700

    add banners.js
    add banners.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import { Header,Content,Footer} from './func-components'
-import Calendar from './class-components'
+import Banner from './banner'
 
- function App(){
-  return(
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-    )
- }
-export default App
+export default function App(){
+  return <Banner/>
+}
