43d35d3de28d1ca1d64d90b9bc1cd2a1d2f13db4
aiam0202 <aiam0202@gmail.com>
2023-09-08 14:47:40 +0700

    add context-content.js
    add context-content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { userContext } from './context' 
+import Content from './context-content'
 import Header from './context-header'
 
-export default function App() 
-{ return (
+export default function App() { 
+  return (
   <userContext.Provider value={'Tom Jerry'}> 
   <Header/>
+  <Content/>
   </userContext.Provider> 
   )
 }
